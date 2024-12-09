4116efc89fde302b6f1a2e528ab8ac693eaed51e
Ozan Malci <ozan41094@gmail.com>
2024-12-09 17:25:50 +0300

    Randevu controlleri ve view'ları güncellendi

---
+++ Controllers/RandevusController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_Odev.Data;
+using Web_Odev.Models;
+
+namespace Web_Odev.Controllers
+{
+    public class RandevusController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public RandevusController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Randevus
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Randevular.ToListAsync());
+        }
+
+        // GET: Randevus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var randevu = await _context.Randevular
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (randevu == null)
+            {
+                return NotFound();
+            }
+
+            return View(randevu);
+        }
+
+        // GET: Randevus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Randevus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,TarihSaat")] Randevu randevu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(randevu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(randevu);
+        }
+
+        // GET: Randevus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var randevu = await _context.Randevular.FindAsync(id);
+            if (randevu == null)
+            {
+                return NotFound();
+            }
+            return View(randevu);
+        }
+
+        // POST: Randevus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,TarihSaat")] Randevu randevu)
+        {
+            if (id != randevu.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(randevu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RandevuExists(randevu.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(randevu);
+        }
+
+        // GET: Randevus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var randevu = await _context.Randevular
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (randevu == null)
+            {
+                return NotFound();
+            }
+
+            return View(randevu);
+        }
+
+        // POST: Randevus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var randevu = await _context.Randevular.FindAsync(id);
+            if (randevu != null)
+            {
+                _context.Randevular.Remove(randevu);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RandevuExists(int id)
+        {
+            return _context.Randevular.Any(e => e.ID == id);
+        }
+    }
+}
