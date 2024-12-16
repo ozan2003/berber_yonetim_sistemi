e113b6df103875c2af39c7dd00bed9eba902ff7e
Ozan Malci <ozan41094@gmail.com>
2024-12-16 19:14:24 +0300

    Salon için controller ekle.

---
+++ Controllers/SalonsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_Odev.Data;
+using Web_Odev.Models;
+
+namespace Web_Odev.Controllers
+{
+    public class SalonsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SalonsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Salons
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Salonlar.ToListAsync());
+        }
+
+        // GET: Salons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salonlar
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+
+            return View(salon);
+        }
+
+        // GET: Salons/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Salons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Isim")] Salon salon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(salon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salon);
+        }
+
+        // GET: Salons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salonlar.FindAsync(id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+            return View(salon);
+        }
+
+        // POST: Salons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Isim")] Salon salon)
+        {
+            if (id != salon.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalonExists(salon.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salon);
+        }
+
+        // GET: Salons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salon = await _context.Salonlar
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (salon == null)
+            {
+                return NotFound();
+            }
+
+            return View(salon);
+        }
+
+        // POST: Salons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var salon = await _context.Salonlar.FindAsync(id);
+            if (salon != null)
+            {
+                _context.Salonlar.Remove(salon);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalonExists(int id)
+        {
+            return _context.Salonlar.Any(e => e.ID == id);
+        }
+    }
+}
